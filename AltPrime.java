d9c69a255714540940c9e807bd35c99297a3f8cc
Prajwal-Gowda-N <prajwal.g12305@gmail.com>
2026-01-30 01:17:33 +0530

    Update AltPrime.java

--- AltPrime.java
+++ AltPrime.java
@@ -1,12 +1,19 @@
 class AltPrime {
     public static void main(String[] args) {
-        int c = 0;
-        for (int n = 2; n <= 50; n++) {
-            boolean p = true;
-            for (int i = 2; i <= Math.sqrt(n); i++)
-                if (n % i == 0) p = false;
-            if (p && ++c % 2 == 1)
-                System.out.print(n + " ");
+        int count = 0;
+
+        for (int n = 2; n <= 100; n++) {
+            int i;
+            for (i = 2; i < n; i++) {
+                if (n % i == 0)
+                    break;
+            }
+
+            if (i == n) {
+                count++;
+                if (count % 2 != 0)
+                    System.out.print(n + " ");
+            }
         }
     }
 }
